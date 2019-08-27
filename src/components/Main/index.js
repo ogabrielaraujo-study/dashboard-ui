d81f26b6cd366c73c2a96cb53600c90d585421e0
Gabriel Araujo <gabriiel66@gmail.com>
2019-08-27 22:49:40 +0000

    hello world

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,15 +6,8 @@ import Grid from '../Grid';
 export default function Main() {
 	return (
 		<main>
-			<Grid cols="5" rows="2">
-				<h1>Box 1</h1>
-				<h1 double="true">Box 2</h1>
-				<h1>Box 3</h1>
-				<h1>Box 4</h1>
-				<h1>Box 5</h1>
-				<h1>Box 6</h1>
-				<h1>Box 7</h1>
-				<h1>Box 8</h1>
+			<Grid cols="1" rows="1">
+				<h1>Hello World</h1>
 			</Grid>
 		</main>
 	);
